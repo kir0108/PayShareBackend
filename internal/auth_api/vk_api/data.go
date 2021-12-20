06831319871a857698fb6487f35b6d7e630aa621
kir0108 <kirill.corschunow@yandex.ru>
2021-12-21 00:18:10 +0300

    Fix parse vk api response

--- internal/auth_api/vk_api/data.go
+++ internal/auth_api/vk_api/data.go
@@ -1,9 +1,13 @@
 package vk_api
 
-type Response struct {
-	Id           int64  `json:"id"`
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	Photo400Orig string `json:"photo_400_orig"`
-	Error        string `json:"error"`
+type ApiResponse struct {
+	Response []struct {
+		Id           int64  `json:"id"`
+		FirstName    string `json:"first_name"`
+		LastName     string `json:"last_name"`
+		Photo400Orig string `json:"photo_400_orig"`
+		Error        string `json:"error"`
+	} `json:"response"`
 }
+
+
