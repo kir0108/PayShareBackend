7408ac637e6f21d5f67f48fe2df8095b35c1ad30
kir0108 <kirill.corschunow@yandex.ru>
2021-12-20 05:12:36 +0300

    Add purchases migrations

---
+++ migrations/000003_create_purchases_table.up.sql
@@ -0,0 +1,20 @@
+create table if not exists purchases
+(
+    id       bigserial           not null,
+    room_id  bigint              not null,
+    p_name   varchar             not null,
+    locate   jsonb  default '{}' not null,
+    owner_id bigint              not null,
+    cost     bigint default 0    not null,
+    constraint purchases_pk
+        primary key (id),
+    constraint purchases_rooms_id_fk
+        foreign key (room_id) references rooms
+            on delete cascade
+);
+
+alter table purchases
+    owner to postgres;
+
+create unique index if not exists purchases_id_uindex
+    on purchases (id);
